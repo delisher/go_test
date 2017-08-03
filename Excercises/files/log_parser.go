5b5c4be7a807b50dd767faec6e3bcfc03b15a59b
Sergey Smirnov <crazyjent@gmail.com>
2017-08-03 19:05:13 +0300

    Another log and print

--- Excercises/files/log_parser.go
+++ Excercises/files/log_parser.go
@@ -9,13 +9,13 @@ import (
 
 func main() {
 	// ErrorNameRE := regexp.MustCompile(`(?:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3}\s).{10,30}`)
-	if file, err := os.Open("D:/Work/LOGS/LOGS/10.32.5.14_drv_1.log"); err == nil {
+	if file, err := os.Open("C:/LOGS/super_big_log.log"); err == nil {
 		defer file.Close()
 		prs := log_parser.NewParser(true)
 		prs.ParseLog(file)
 		for k, _ := range prs.Errors {
 			e := prs.Errors[k]
-			fmt.Printf("%v:\n\t%v\n\t%v раз\n\t\n", e.Name, e.FullErr, e.Number)
+			fmt.Printf("%v errors:\n\n%v\n\n\n", e.Number, e.FullErr)
 		}
 	} else {
 		log.Fatal(err)

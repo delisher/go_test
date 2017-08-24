2a6a0d08ead0f75d2e059a91e22d41693fc02e16
Sergey Smirnov <crazyjent@gmail.com>
2017-08-25 02:27:03 +0300

    FullErrText in own file some optimizations and simplifications

--- Excercises/files/log_parser.go
+++ Excercises/files/log_parser.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"logstats"
 	"os"
 )
@@ -11,10 +10,6 @@ var (
 )
 
 func main() {
-	// ErrorNameRE := regexp.MustCompile(`(?:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3}\s).{10,30}`)
-	// for k, v := range os.Args {
-	// 	fmt.Printf("%v => %v\n", k, v)
-	// }
 	lp := log_parser.NewParsersControl(true, os.Args[1:len(os.Args)])
 	lp.ToConsole()
 }
